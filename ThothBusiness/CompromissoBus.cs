9491de99b2dc32cba92fb6826db6e1ec500e09b1
MScorza2023 <121317675+MScorza2023@users.noreply.github.com>
2025-04-24 21:17:34 -0300

    Add files via upload

---
+++ ThothBusiness/CompromissoBus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ThothEntity;
+using ThothDataAccess;
+using System.Collections.Generic;
+
+namespace ThothBusiness
+{
+    public class CompromissoBus
+    {
+        public bool AlterarCompromisso(Compromisso compromisso)
+        {
+            CompromissoDal compromissoDal = new CompromissoDal();
+            return compromissoDal.AlterarCompromisso(compromisso);
+        }
+
+        public bool ExcluirCompromisso(int idUsuario, int idCompromisso)
+        {
+            CompromissoDal compromissoDal = new CompromissoDal();
+            return compromissoDal.ExcluirCompromisso(idUsuario, idCompromisso);
+        }
+
+        public bool ExisteOutroCompromisso(int idUsuario, string dataAgenda, int idCompromisso)
+        {
+            CompromissoDal compromissoDal = new CompromissoDal();
+            return compromissoDal.ExisteCompromisso(idUsuario, dataAgenda, idCompromisso);
+        }
+
+        public int InserirCompromisso(Compromisso compromisso)
+        {
+            CompromissoDal compromissoDal = new CompromissoDal();
+            return compromissoDal.InserirCompromisso(compromisso);
+        }
+
+        public List<Compromisso> ListarCompromissos(int idUsuario, string dataAgenda)
+        {
+            CompromissoDal comprDal = new CompromissoDal();
+            return comprDal.ListarCompromissos(idUsuario, dataAgenda);
+        }
+    }
+}
---
+++ ThothBusiness/UsuarioBus.cs
@@ -0,0 +1,89 @@
+﻿using ThothEntity;
+using ThothDataAccess;
+using System.Collections.Generic;
+
+namespace ThothBusiness
+{
+    public class UsuarioBus
+    {
+        public bool AlterarUsuario(Usuario usuario)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.AlterarUsuario(usuario);
+        }
+
+        public bool ExcluirUsuario(int idUsuario)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.ExcluirUsuario(idUsuario);
+        }
+
+        public int InserirUsuario(Usuario usuario)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.IncluirUsuario(usuario);
+        }
+
+        public List<Usuario> ListarUsuarios()
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.ListarUsuarios();
+        }
+
+        public bool LoginExiste(string login, string operacao, int idUsuario = 0)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            bool jaExiste = usuarioDal.LoginJaExiste(login, out int id);
+            if (operacao == "I")
+            {
+                return jaExiste;
+            }
+            if (operacao == "A")
+            {
+                if (jaExiste && id == idUsuario)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool NomeExiste(string nome, string operacao, int idUsuario = 0)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            bool jaExiste = usuarioDal.NomeJaExiste(nome, out int id);
+            if (operacao == "I")
+            {
+                return jaExiste;
+            }
+            if (operacao == "A")
+            {
+                if (jaExiste && id == idUsuario)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string ObterSenha(int idUsuario)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.ObterSenha(idUsuario);
+        }
+
+        public int ValidarLogin(string login)
+        {
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.ObterIdUsuario(login);
+        }
+
+        public bool ValidarSenha(int idUsuario, string senha, out string nome)
+        {
+            nome = string.Empty;
+
+            UsuarioDal usuarioDal = new UsuarioDal();
+            return usuarioDal.ValidarSenha(idUsuario, senha, out nome);
+        }
+    }
+}
